1cd389f886a37c9161859843c0b3fc7fec08c965
Kristina047 <ch.drobot@gmail.com>
2024-11-12 17:04:03 +0300

    Много задач — один проект

--- src/Main.java
+++ src/Main.java
@@ -10,39 +10,39 @@ public class Main {
 
 
         System.out.println("Задача 2");
-        dog = dog + 4;
+        dog += 4;
         System.out.println(dog);
-        cat = cat + 4;
+        cat += 4;
         System.out.println(cat);
-        paper = paper + 4;
+        paper += 4;
         System.out.println(paper);
 
 
         System.out.println("Задача 3");
-        dog = dog - 3.5;
+        dog -= 3.5;
         System.out.println (dog);
-        cat = cat -1.6;
+        cat -= 1.6;
         System.out.println (cat);
-        paper = paper - 7639;
+        paper -= 7639;
         System.out.println (paper);
 
 
         System.out.println("Задача 4");
         var friend = 19;
         System.out.println (friend);
-        friend = (friend*2);
+        friend *= 2;
         System.out.println (friend);
-        friend = (friend/7);
+        friend/= 7;
         System.out.println (friend);
 
         System.out.println("Задача 5");
         var frog = 3.5;
         System.out.println (frog);
-        frog = 3.5*10;
+        frog *= 10;
         System.out.println (frog);
-        frog = frog/3.5;
+        frog /=3.5;
         System.out.println (frog);
-        frog = frog+4;
+        frog +=4;
         System.out.println (frog);
 
         System.out.println("Задача 6");
